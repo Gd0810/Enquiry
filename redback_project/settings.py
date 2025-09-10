805043f9df136a43f37bf83366dc852ce6cc1b5d
Gd0810 <gd8102000@gmail.com>
2025-09-10 12:22:07 +0530

    static files updated

--- redback_project/settings.py
+++ redback_project/settings.py
@@ -84,8 +84,15 @@ WSGI_APPLICATION = 'redback_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql1',         # Change to your database name
+        'USER': 'enquires',                 # Default XAMPP user
+        'PASSWORD': 'EfrePQ3SLAhq6mDp0ylL',                 # Default XAMPP password is empty
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
